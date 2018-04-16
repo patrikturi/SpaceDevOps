40a6fb7744a03bf891b88e7033f5b7e8488a2cdb
Patrik Turi <tpphysik@gmail.com>
2018-04-17 00:36:20 +0200

    Implement CameraController

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform m_Target;
+
+	private Vector3 offset = new Vector3(0, 5.5f, -10f);
+	private float m_SmoothDuration = 0.4f;
+
+	private Vector3 m_Velocity = Vector3.zero;
+
+	void FixedUpdate () {
+		Vector3 desiredPos = m_Target.position + m_Target.transform.rotation * offset;
+		Vector3 newPos = Vector3.SmoothDamp (transform.position, desiredPos, ref m_Velocity, m_SmoothDuration);
+
+		transform.position = newPos;
+		transform.LookAt (m_Target.position);
+	}
+}
