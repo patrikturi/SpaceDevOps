461167475c27ec6f3b8b27f49cc4434e2cf98b82
Patrik Turi <tpphysik@gmail.com>
2018-05-01 23:23:08 +0200

    Switch to First Person Camera and back

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	public Transform m_Target;
+	public GameObject m_Target;
 
+	private string CAMERA_BUTTON = "Camera";
 	private Vector3 POS_OFFSET = new Vector3(0, 5.5f, -10f);
 	private const float POS_SMOOTHING_DURATION = 0.4f;
 	private float UP_SMOOTHING_STEP;
 
+	private MeshRenderer m_TargetRenderer;
 	private Vector3 m_CameraUp;
 	private Vector3 m_Velocity = Vector3.zero;
+	// false means it's Third person camera
+	private bool m_FirstPersonCamera = false;
 
 	void Start() {
+		m_TargetRenderer = m_Target.GetComponent<MeshRenderer> ();
 		UP_SMOOTHING_STEP = 1f * Time.fixedDeltaTime;
 		m_CameraUp = m_Target.transform.up;
 	}
 
 	void FixedUpdate () {
-		Vector3 desiredPos = m_Target.position + m_Target.transform.rotation * POS_OFFSET;
-		Vector3 newPos = Vector3.SmoothDamp (transform.position, desiredPos, ref m_Velocity, POS_SMOOTHING_DURATION,
-			Mathf.Infinity, Time.fixedDeltaTime);
+		Transform targetTr = m_Target.transform;
+		if (Input.GetButtonDown (CAMERA_BUTTON)) {
+			m_FirstPersonCamera = !m_FirstPersonCamera;
+		}
 
-		// Must keep the camera Up vector moving otherwise the camera can 'flip' around
-		m_CameraUp = Vector3.Lerp (m_CameraUp, m_Target.transform.up, UP_SMOOTHING_STEP);
+		if (m_FirstPersonCamera) {
+			// TODO: will need a better solution for Networked Multiplayer, see:
+			// https://answers.unity.com/questions/63261/network-restrictive-rendering.html
+			m_TargetRenderer.enabled = false;
+			transform.position = targetTr.position;
+			transform.rotation = targetTr.rotation;
+			m_CameraUp = targetTr.up; // Needed to transition smoothly into Third Person Camera
+		} else { // Third Person Camera
+			m_TargetRenderer.enabled = true;
+			Vector3 desiredPos = targetTr.position + targetTr.rotation * POS_OFFSET;
+			Vector3 newPos = Vector3.SmoothDamp (transform.position, desiredPos, ref m_Velocity, POS_SMOOTHING_DURATION,
+				                Mathf.Infinity, Time.fixedDeltaTime);
 
-		transform.position = newPos;
-		transform.LookAt (m_Target.position, m_CameraUp);
+			// Must keep the camera Up vector moving otherwise the camera can 'flip' around
+			m_CameraUp = Vector3.Lerp (m_CameraUp, targetTr.up, UP_SMOOTHING_STEP);
+
+			transform.position = newPos;
+			transform.LookAt (targetTr.position, m_CameraUp);
+		}
 	}
 }
