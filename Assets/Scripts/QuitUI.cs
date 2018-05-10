403a44a6d150131b2bdf7ccff022395e493b23e8
Patrik Turi <tpphysik@gmail.com>
2018-05-10 22:44:28 +0200

    Quit with ESC

---
+++ Assets/Scripts/QuitUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitUI : MonoBehaviour {
+
+	void OnGUI() {
+		Event e = Event.current;
+		if (e.type == EventType.KeyDown) {
+			if (e.keyCode == KeyCode.Escape) {
+				gameObject.SetActive (false);
+			} else if (e.keyCode == KeyCode.Return || e.keyCode == KeyCode.KeypadEnter) {
+				Application.Quit ();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject m_HelpPanel;
+	public GameObject m_QuitPanel;
+
+	void Awake () {
+		Cursor.visible = false;
+	}
+
+	void OnGUI() {
+		Event e = Event.current;
+		if (e.type == EventType.KeyDown) {
+			if (e.keyCode == KeyCode.F1) {
+				m_HelpPanel.SetActive (true);
+			} else if (e.keyCode == KeyCode.Escape) {
+				m_QuitPanel.SetActive (true);
+			}
+		} else if (e.type == EventType.KeyUp) {
+			if (e.keyCode == KeyCode.F1) {
+				m_HelpPanel.SetActive (false);
+			}
+		}
+	}
+}
