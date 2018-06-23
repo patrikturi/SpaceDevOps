0f6c22708407726385cf41b6b9db0c1bae75bca3
Patrik Turi <tpphysik@gmail.com>
2018-06-23 21:29:44 +0200

    Implement shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private const int BULLET_DAMAGE = 20;
+
+	void OnTriggerEnter(Collider other) {
+
+		GameObject hitObject = other.gameObject;
+		// Ship hull
+		Health health = hitObject.GetComponent<Health>();
+
+		// Ship wings
+		// TODO: should hitting the wing take less damage?
+		if(health == null && hitObject.name.Contains("Wing")) {
+			GameObject parentObject = hitObject.transform.parent.gameObject;
+			health = parentObject.GetComponent<Health> ();
+		}
+
+		if (health != null) {
+			health.takeDamage (BULLET_DAMAGE);
+		}
+
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour {
+
+	// TODO: bullet pooling
+
+	public GameObject m_BulletPrefab;
+	public Transform m_BulletSpawnRight;
+	public Transform m_BulletSpawnLeft;
+
+	private const float BULLET_SPEED = 30f;
+	private const float BULLET_DURATION = 3.5f;
+	private const float COOLDOWN_DURATION = 0.25f;
+	private const int BULLET_LAYER = 8;
+
+	private float fireEnabledTimeStamp = -1f;
+
+	// Update is called once per frame
+	void Update () {
+		float fireInput = Input.GetAxis ("Fire");
+
+		if (fireInput > 0.1f && fireEnabledTimeStamp <= Time.time) {
+			fireAll();
+			fireEnabledTimeStamp = Time.time + COOLDOWN_DURATION;
+		}
+	}
+
+	private void fireAll() {
+		fireSingle (m_BulletSpawnRight);
+		fireSingle (m_BulletSpawnLeft);
+	}
+
+	private void fireSingle(Transform spawn) {
+		var bullet = (GameObject)Instantiate (m_BulletPrefab, spawn.position, spawn.rotation);
+		bullet.GetComponent<Rigidbody> ().velocity = transform.forward * BULLET_SPEED;
+		bullet.layer = BULLET_LAYER;
+
+		Destroy (bullet, BULLET_DURATION);
+	}
+}
