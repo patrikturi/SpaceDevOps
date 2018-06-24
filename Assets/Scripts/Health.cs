2f44c0ab1224785b235a12b823e4dff95b663c7a
Patrik Turi <tpphysik@gmail.com>
2018-06-24 16:30:27 +0200

    Create death explosion

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class Health : MonoBehaviour {
 
 	public RectTransform m_HealthBar;
+	public GameObject m_ExplosionPrefab;
 
 	private float INITIAL_WIDTH;
 	private const int MAX_HEALTH = 100;
+	private const float EXPLOSION_LIFETIME = 4.5f;
+	private const float EXPLOSION_BURST_TIME = 0.5f;
+
 	private int m_CurrentHealth = MAX_HEALTH;
 
 	void Awake() {
@@ -18,8 +22,26 @@ public class Health : MonoBehaviour {
 		m_CurrentHealth -= amount;
 		if (m_CurrentHealth <= 0) {
 			m_CurrentHealth = 0;
-			Debug.Log("Dead");
+			die ();
 		}
 		m_HealthBar.sizeDelta = new Vector2(INITIAL_WIDTH*m_CurrentHealth/MAX_HEALTH, m_HealthBar.sizeDelta.y);
+	}
+
+	GameObject explosion;
+
+	private void die() {
+		explosion = (GameObject)Instantiate (m_ExplosionPrefab, transform.position + transform.forward*0.75f, transform.rotation);
+
+		Rigidbody body = GetComponent<Rigidbody> ();
+		Rigidbody expBody = explosion.GetComponent<Rigidbody> ();
+		expBody.velocity = body.velocity;
+
+		Destroy (explosion, EXPLOSION_LIFETIME);
+
+		Invoke ("SetInactive", EXPLOSION_BURST_TIME-0.2f);
+	}
+
+	private void SetInactive() {
+		gameObject.SetActive (false);
 	}
 }
