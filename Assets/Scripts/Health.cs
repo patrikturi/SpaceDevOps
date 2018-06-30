fbbd6293aeb52d0f94aacc6b204195a7b6bcc39b
Patrik Turi <tpphysik@gmail.com>
2018-06-30 23:51:55 +0200

    Add fire particles on takeDamage

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,6 +6,7 @@ public class Health : MonoBehaviour {
 
 	public RectTransform m_HealthBar;
 	public GameObject m_ExplosionPrefab;
+	public GameObject m_HitParticlesHost;
 
 	private float INITIAL_WIDTH;
 	private const int MAX_HEALTH = 100;
@@ -13,9 +14,15 @@ public class Health : MonoBehaviour {
 	private const float EXPLOSION_BURST_TIME = 0.5f;
 
 	private int m_CurrentHealth = MAX_HEALTH;
+	private ParticleSystem m_ParticleSystem;
 
 	void Awake() {
 		INITIAL_WIDTH = m_HealthBar.sizeDelta.x;
+		m_ParticleSystem = m_HitParticlesHost.GetComponent<ParticleSystem> ();
+	}
+
+	void OnEnable() {
+		m_HitParticlesHost.transform.parent = transform;
 	}
 
 	public void Reset() {
@@ -27,6 +34,8 @@ public class Health : MonoBehaviour {
 
 		if (Debug.isDebugBuild && e.type == EventType.KeyUp && e.control && e.keyCode == KeyCode.D) {
 			takeDamage (MAX_HEALTH);
+		} else if (Debug.isDebugBuild && e.type == EventType.KeyUp && e.control && e.keyCode == KeyCode.X) {
+			takeDamage (20);
 		}
 	}
 
@@ -37,6 +46,7 @@ public class Health : MonoBehaviour {
 
 		if (m_CurrentHealth > amount) {
 			setHealth (m_CurrentHealth - amount);
+			m_ParticleSystem.Emit (amount/10+1);
 		} else {
 			die ();
 		}
@@ -49,6 +59,8 @@ public class Health : MonoBehaviour {
 
 	private void die() {
 		setHealth (0);
+		// Detach fire particles from the ship so they don't disappear when the ship is deactivated
+		m_HitParticlesHost.transform.parent = null;
 		GameObject explosion = (GameObject)Instantiate (m_ExplosionPrefab, transform.position + transform.forward*0.75f, transform.rotation);
 
 		Rigidbody body = GetComponent<Rigidbody> ();
